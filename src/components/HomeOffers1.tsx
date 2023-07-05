d5912c119bd00f496741c4452ace62916eb479bb
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2023-07-05 18:33:05 +0530

    dashboardUI

--- src/components/HomeOffers1.tsx
+++ src/components/HomeOffers1.tsx
@@ -6,29 +6,18 @@ import React from "react";
 import Topbar from "./Topbar";
 import { useNetwork } from "wagmi";
 import { useChainModal } from "@rainbow-me/rainbowkit";
+import Myzixins from "./Myzixins";
 const HomeOffers1: FunctionComponent = () => {
   const { openChainModal } = useChainModal();
   const { chain} = useNetwork()
   return (
-    <div className={styles.homeoffers1}>
-      <div className={styles.homeoffers1Child} />
-      <div className={styles.text}>
-        <div className={styles.profile}>Profile</div>
-        <div className={styles.breadcrumbitemprevious}>
-          <div className={styles.breadcrumb}>
-            <span>
-              <span>{`Pages `}</span>
-            </span>
-            <span className={styles.profile1}>
-              <span>{` `}</span>
-              <span className={styles.profile2}>/ Profile</span>
-            </span>
-          </div>
-        </div>
-      </div>
-      <Topbar />
-      <div className={styles.tokenview}>
-        <div className={styles.background}>
+    <>
+    <div className={styles.whole}>
+    {/* <div> */}
+    <Myzixins/>
+    {/* </div> */}
+    <div>
+    <div className={styles.tokenview}>
           <div className={styles.tabs}>
             <div className={styles.teams} onClick={() => {}}>
               <div className={styles.widthStructure}>
@@ -51,39 +40,32 @@ const HomeOffers1: FunctionComponent = () => {
                     <b className={styles.text4}>Zìxìn</b>
                   </div>
                 </div>
-                <div className={styles.minwidth}>
-                  <div className={styles.content} />
-                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className={styles.cards}>
-          <div className={styles.card3}>
-            <Profilecard
-              name="Linkedin Auth"
-              description="linkedin"
-              imgsrc="/image5.svg"
-            />
-          </div>
-          <div className={styles.card2}>
-            <Profilecard
-              name="Github Auth"
-              description="Github"
-              imgsrc="/image3.svg"
-            />
-          </div>
-          <div className={styles.card1}>
-            <Profilecard
-              name="Google Auth"
-              description="Google"
-              imgsrc="/image6@2x.png"
-            />
+      </div>
+    </div>
+    <div className={styles.homeoffers1}>    
+      <div className={styles.homeoffers1Child} />
+      <div className={styles.text}>
+        <div className={styles.profile}>Profile</div>
+        <div className={styles.breadcrumbitemprevious}>
+          <div className={styles.breadcrumb}>
+            <span>
+              <span>{`Pages `}</span>
+            </span>
+            <span className={styles.profile1}>
+              <span>{` `}</span>
+              <span className={styles.profile2}>/ Profile</span>
+            </span>
           </div>
         </div>
       </div>
+      <Topbar />
       <Sidebar value={0} />
     </div>
+    </div>
+    </>
   );
 };
 
---
+++ src/components/Myzixins.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { FunctionComponent } from "react";
+import styles from "../../styles/Mynfts.module.css";
+import Profilecard from "./Profilecard";
+
+const Myzixins: FunctionComponent = () => {
+  const [profiles, setProfiles] = useState([
+    {
+      name: "Linkedin Auth",
+      description: "linkedin",
+      imgsrc: "/image5.svg",
+    },
+    {
+      name: "Github Auth",
+      description: "Github",
+      imgsrc: "/image3.svg",
+    },
+    {
+      name: "Google Auth",
+      description: "Google",
+      imgsrc: "/image6@2x.png",
+    },
+    {
+        name: "Github Auth",
+        description: "Github",
+        imgsrc: "/image3.svg",
+      },
+      {
+        name: "Linkedin Auth",
+        description: "linkedin",
+        imgsrc: "/image5.svg",
+      },
+ 
+  ]);
+
+  const addProfile = () => {
+    const newProfile = {
+      name: "New Profile",
+      description: "New Description",
+      imgsrc: "/placeholder-image.png",
+    };
+    setProfiles([...profiles, newProfile]);
+  };
+
+  return (
+    <div className={styles.whole}>
+      <div className={styles.back}>
+        <div className={styles.cards}>
+          {profiles.map((profile, index) => (
+            <div key={index} className={styles.zixins}>
+              <Profilecard
+                name={profile.name}
+                description={profile.description}
+                imgsrc={profile.imgsrc}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Myzixins;
